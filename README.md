c3d5b901f34ba5872085f426e520c5dfa9f26091
thiswillbeyourgithub <github@32mail.33mail.comm>
2021-02-28 20:58:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Hapax Predator
+
+# In a few words :
+* When summoned, the Hapax Predator will show you all the words found in the current selection of cards, ordered by frequency. This allows to find the least used words, which are propably misssssspelings.
+* The words are separated using a small series of python scripts and will very probably missbehave because of accents and such, don't hesitate to open an issue in case anything goes wrong.
+
+![picture TODO](HapaxPredator.jpg)
+
+# FAQ :
+* **How can I use it ?** Open the browser > select cards > click on `Edit` then `Hapax Predator`
+* **Why this name?** It's a play on words, an hapax is a word that appears only once in a text and an apex predator is the one on top of the food chain.
+* **How can I install it?** Go to this page : todo
+* **Should I open an issue if X?** Of course please do, I am very eager to get feedbacks and am very early to all this.
+* **What happenned to my accents?** I decided to ignore as many accents as possible (for example `é` got turned into `e`) because I create way too many cards to care about them.
+* **Why did you even think this was a problem that needed to be solve?!** Fixing misspellings is a first step towards managing NLP tasks in anki. Stay tunend.
---
+++ __init__.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+# Hapax Predator
+# see LICENSE file
+
+
+#  Released under the GNU General Public License v3.
+#  Copyright (C) - 2020 - user "thiswillbeyourgithub" of the website "github".
+#  This file is the code for Hapax Predator : an tool that orders your words
+#  by frequency to help find misspellings and pave the way for NLP tasks.
+#  
+#  Hapax Predator is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  
+#  Hapax Predator is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with Hapax Predator.  If not, see <https://www.gnu.org/licenses/>.
+#  
+#  for more information or to get the latest version go to :
+#  https://github.com/thiswillbeyourgithub/Clozolkor
+#  Version : ferbuary 2021
+#  
+#  credits due to Glutanimate, his code for the addon "Batch Editing" was 
+#  the basis for the QT code that I used below, as I have not yet learned QT.
+
+
+import re
+import pprint
+from collections import Counter
+
+from aqt.qt import *
+from aqt.utils import tooltip
+from anki.hooks import addHook
+
+
+class HPDialog(QDialog):
+    def __init__(self, browser, nids):
+        QDialog.__init__(self, parent=browser)
+        self.browser = browser
+        self.nids = nids
+        self._setup()
+
+    def _setup(self):
+        browser = self.browser
+        nids = self.nids
+        fld = "Body"
+        mw = browser.mw
+        mw.checkpoint("Hapax Predator")
+        cnt = 0
+        all_texts=[]
+        for nid in nids:
+            note = mw.col.getNote(nid)
+            all_texts.extend(note.fields)
+            cnt += 1
+
+        all_text = " ".join(all_texts)
+        all_text = re.sub("::|/", " ", all_text)
+        all_text = re.sub("title|style|class|height|source|width|paste|figure", " ", all_text)
+        all_text = re.sub("{{c\d+::|}}", "", all_text)
+        all_text = re.sub("é|è|ê", "e", all_text)
+        all_text = re.sub("à", "a", all_text)
+        all_text = all_text.lower()
+        alphaNumeric = re.compile("[a-zA-ZÀ-ú][a-zA-ZÀ-ú]{4,25}")
+        words = [w for w in alphaNumeric.findall(all_text)]
+        count = Counter(words)
+        doneText = str(pprint.pformat(count))
+        tlabel = QLabel("Hapax Predator :\nHere are the words from your "\
+            + "cards  by frequency\nUse it to correct mistakes")
+
+        top_hbox = QHBoxLayout()
+        top_hbox.addWidget(tlabel)
+        top_hbox.insertStretch(1, stretch=1)
+
+        self.tedit = QPlainTextEdit(doneText)
+        self.tedit.setTabChangesFocus(True)
+        button_box = QDialogButtonBox(Qt.Horizontal, self)
+        close_btn = button_box.addButton("&Close",
+                                         QDialogButtonBox.RejectRole)
+        close_btn.clicked.connect(self.close)
+
+        bottom_hbox = QHBoxLayout()
+        bottom_hbox.addWidget(button_box)
+
+        vbox_main = QVBoxLayout()
+        vbox_main.addLayout(top_hbox)
+        vbox_main.addWidget(self.tedit)
+        vbox_main.addLayout(bottom_hbox)
+        self.setLayout(vbox_main)
+        self.tedit.setFocus()
+        self.setMinimumWidth(540)
+        self.setMinimumHeight(400)
+        self.setWindowTitle("Hapax Predator")
+
+def HP(browser):
+    nids = browser.selectedNotes()
+    if not nids:
+        tooltip("No cards selected.")
+        return
+    dialog = HPDialog(browser, nids)
+    dialog.exec_()
+
+def setupMenu(browser):
+    menu = browser.form.menuEdit
+    menu.addSeparator()
+    a = menu.addAction('Hapax Predator')
+    a.triggered.connect(lambda _, b=browser: HP(b))
+
+addHook("browser.setupMenus", setupMenu)
